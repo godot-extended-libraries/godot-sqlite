8fa31bf9f61b7a06d9486f73bc5c84074e976531
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-01-24 05:57:32 -0800

    Rearrange SQlite headers.

--- sqlite.h
+++ sqlite.h
@@ -5,8 +5,8 @@
 #include "core/reference.h"
 
 // SQLite3
-#include "thirdparty/sqlite/sqlite3.h"
 #include "thirdparty/sqlite/spmemvfs.h"
+#include "thirdparty/sqlite/sqlite3.h"
 
 class SQLite : public Reference {
 	GDCLASS(SQLite, Reference);
@@ -19,14 +19,20 @@ private:
 	spmemvfs_db_t p_db;
 	bool memory_read;
 
+	sqlite3_stmt *prepare(const char *statement);
+	Array fetch_rows(String query, PoolStringArray args, int result_type = RESULT_BOTH);
+	Dictionary parse_row(sqlite3_stmt *stmt, int result_type);
+	sqlite3 *get_handler() { return (memory_read ? p_db.handle : db); }
+
+protected:
+	static void _bind_methods();
+
 public:
 	enum {
 		RESULT_BOTH = 0,
 		RESULT_NUM,
 		RESULT_ASSOC
 	};
-
-	static void _bind_methods();
 
 	// constructor
 	SQLite();
@@ -44,11 +50,5 @@ public:
 	Array fetch_array_with_args(String statement, PoolStringArray args);
 	Array fetch_assoc(String statement);
 	Array fetch_assoc_with_args(String statement, PoolStringArray args);
-
-private:
-	sqlite3_stmt* prepare(const char* statement);
-	Array fetch_rows(String query, PoolStringArray args, int result_type = RESULT_BOTH);
-	Dictionary parse_row(sqlite3_stmt *stmt, int result_type);
-	sqlite3* get_handler() { return (memory_read ? p_db.handle : db); }
 };
 #endif
