b4c355ec429dc7869ca16f8723753a59f8433c9d
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-11-27 19:38:50 -0800

    Syntax check.

--- demo/SQLite/game_highscore.gd
+++ demo/SQLite/game_highscore.gd
@@ -9,24 +9,24 @@ onready var open = false;
 func _ready():
 	# Create gdsqlite instance
 	db = SQLite.new();
-	
+
 	# Open the database
 	if (not db.open("user://player_stats.sqlite")):
 		return;
-	
+
 	open = true;
-	
+
 	# Create table
 	var query = "CREATE TABLE IF NOT EXISTS highscore (id INTEGER PRIMARY KEY, score INTEGER NOT NULL);";
 	if (not db.query(query)):
 		return;
-	
+
 	# Retrieve current highscore
 	var rows = db.fetch_array("SELECT id, score FROM highscore LIMIT 1;");
 	if (rows and not rows.empty()):
 		row_id = rows[0]['id'];
 		highscore = rows[0]['score'];
-	
+
 	# Test
 	set_highscore(1000);
 	set_highscore(2000);
@@ -42,10 +42,10 @@ func _exit_tree():
 func set_highscore(score):
 	if not open:
 		return;
-	
+
 	# Update highscore
 	highscore = score;
-	
+
 	# Execute sql syntax
 	if (row_id > 0):
 		db.query_with_args("UPDATE highscore SET score=? WHERE id=?;", [highscore, row_id]);
@@ -56,11 +56,11 @@ func set_highscore(score):
 func get_highscore():
 	if not open:
 		return;
-	
+
 	# Retrieve highscore from database
 	var rows = db.fetch_array_with_args("SELECT score FROM highscore WHERE id=? LIMIT 1;", [row_id]);
 	if (rows and not rows.empty()):
 		highscore = rows[0]['score'];
-	
+
 	# Return the highscore
 	return highscore;
