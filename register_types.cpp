f0423a83164d08814b9aa5aaa5add541a51df193
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-11 20:10:13 -0700

    Apply clang format.

--- register_types.cpp
+++ register_types.cpp
@@ -4,15 +4,15 @@
 #include "sqlite.h"
 
 void initialize_sqlite_module(ModuleInitializationLevel p_level) {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SERVERS) {
-		return;
-	}
+  if (p_level != MODULE_INITIALIZATION_LEVEL_SERVERS) {
+    return;
+  }
   ClassDB::register_class<SQLite>();
   ClassDB::register_class<SQLiteQuery>();
 }
 
 void uninitialize_sqlite_module(ModuleInitializationLevel p_level) {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SERVERS) {
-		return;
-	}
+  if (p_level != MODULE_INITIALIZATION_LEVEL_SERVERS) {
+    return;
+  }
 }
